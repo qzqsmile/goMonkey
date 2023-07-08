e8c0c8144a42825ef51c196ceefa720cdeac281f
Q <qzqsmile@gmail.com>
2023-07-08 23:11:34 +0800

    add identity expression

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "goMokeney/src/token"
+import (
+	"bytes"
+	"goMokeney/src/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -16,6 +20,11 @@ type Expression interface {
 	expressionNode()
 }
 
+type ExpressionStatement struct{
+	Token token.Token
+	Expression Expression
+}
+
 type Program struct {
 	Statements []Statement
 }
@@ -26,6 +35,15 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+func (p *Program) String() string{
+	var out bytes.Buffer
+
+	for _, s := range p.Statements{
+		out.WriteString(s.String())
+	}
+	return out.String()
 }
 
 type LetStatement struct {
@@ -48,10 +66,51 @@ func (ls *LetStatement) statementNode() {}
 
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() string{
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral()+" ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil{
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	
+	return out.String()
+}
+
 func (ls *ReturnStatement) statementNode() {}
 
 func (ls *ReturnStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (rs *ReturnStatement) String() string{
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil{
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
+
 func (i *Identifier) expressionNode() {}
 
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (es *ExpressionStatement) statementNode() {}
+
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() string{
+	if es.Expression != nil{
+		return es.Expression.String()
+	}
+	return ""
+}
+
+func (i *Identifier) String() string {return  i.Value}
--- src/parser/parser.go
+++ src/parser/parser.go
@@ -7,20 +7,54 @@ import (
 	"goMokeney/src/token"
 )
 
+type(
+	prefixParseFn func() ast.Expression
+	infixParseFn func(ast.Expression) ast.Expression
+)
+
+const (
+	_ int = iota 
+	LOWEST
+	EQUALS 
+	LESSGREATER
+	SUM 
+	PRODUCT
+	PREFIX
+	CALL
+)
+
 type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
 	errors    []string
+
+	prefixParseFns map[token.TokenType]prefixParseFn 
+	infixParseFns map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l,
 		errors: []string{}}
 	// Read two tokens, so curToken and peekToken are both set
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	p.registerPrefix(token.IDENT, p.parseIdentifier)
+
 	p.nextToken()
 	p.nextToken()
 	return p
+}
+
+// func (p *Parser) parseIdentifier() ast.Expression {
+// 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+// }
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn)  {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn){
+	p.infixParseFns[tokenType] = fn
 }
 
 func (p *Parser) Errors() []string {
@@ -59,8 +93,32 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
+		return p.parseExpressionStatement()
+	}
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.curToken}
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON){
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) parseIdentifier() ast.Expression{
+	return &ast.Identifier{Token:p.curToken, Value:p.curToken.Literal}
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression{
+	prefix := p.prefixParseFns[p.curToken.Type]
+	if prefix == nil{
 		return nil
 	}
+	leftExp := prefix()
+
+	return leftExp
 }
 
 func (p *Parser) parseLetStatement() *ast.LetStatement {
